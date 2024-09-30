9bfaa52febc3c70a33f3795d5882262c593e3b02
eliyan-mu <eliyanmousazadeh@hilma.tech>
2024-09-30 17:25:52 +0300

    final sugnup

--- sigUp/signUP.js
+++ sigUp/signUP.js
@@ -1,27 +1,32 @@
-const firstname = document.getElementById("fname").value;
-const lastname = document.getElementById("pass").value;
-const password = document.getElementById("lname").value;
-const user = {
-    "firstname":firstname,
-    "lastname":lastname,
-    "password":password
-    
-}
 
-if(!localStorage["users"]){
-    localStorage.setItem("users",JSON.stringify([]));
-}
 
-console.log('user: ', user);
-let userArray=JSON.parse(localStorage["users"]);
-userArray.push(user);
-localStorage["users"]=JSON.stringify(userArray);
 
-let arrUersname=[];
+
+let arrUersname = [];
 
 const form = document.getElementById("myForm");
-form.addEventListener("submit",userid)
-function userid() {
+// form.addEventListener("submit",userid)
 
+
+function signup() {
+    const firstname = document.getElementById("fname").value;
+    const lastname = document.getElementById("pass").value;
+    const password = document.getElementById("lname").value;
+    const user = {
+        "firstname": firstname,
+        "lastname": lastname,
+        "password": password
+
+    }
+
+    if (!localStorage.getItem("users")) {
+        localStorage.setItem("users", JSON.stringify([]));
+    }
+    console.log('user: ', user);
+    let userArray = JSON.parse(localStorage.getItem("users"));
+    userArray.push(user);
+    localStorage.setItem("users", JSON.stringify(userArray));
 }
+let submit = document.getElementById("submit");
+submit.addEventListener("click", signup);
 
